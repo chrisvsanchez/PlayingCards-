76f5e0bd4296cfd01ce3b8b63725b79d8cd2fd95
chrisvsanchez <chrisvsanchez@gmail.com>
2021-07-28 16:09:06 -0400

    added styled component api

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components/macro";
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <div>
+      fdfdfd
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
 };
 
 export default NavBar;
