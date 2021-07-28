d5a32d332f89acd591a7d1f84ed815f1708a63c4
chrisvsanchez <chrisvsanchez@gmail.com>
2021-07-28 15:55:48 -0400

    created Home, HowToPlay and Navbar components and set up APP.js routes with react router DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HowToPlay from "./Components/HowToPlay";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
+import NotFound from "./Components/NotFound";
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/How-to-play" component={HowToPlay} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
