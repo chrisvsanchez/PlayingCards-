87847bd6fca1dae3c7905b701e4b484f87233cc5
chrisvsanchez <chrisvsanchez@gmail.com>
2021-07-28 22:48:13 -0400

    updated navbar with some styled component css

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styled from "styled-components/macro";
 
 const NavBar = ({ Home, HowToPlay }) => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/how-to-play">How To Play</Link>
-    </nav>
+    <Wrapper>
+      <ULWrapper>
+        <NavLink to="/" activeClassName="current" exact>
+          <li>Home</li>
+        </NavLink>
+        <NavLink to="/how-to-play" activeClassName="current" exact>
+          <li>How to Play</li>
+        </NavLink>
+      </ULWrapper>
+    </Wrapper>
   );
 };
 
 export default NavBar;
+const Wrapper = styled.div`
+  flex: 1;
+  height: 50px;
+  background-color: lavender;
+`;
+const ULWrapper = styled.ul`
+  font-family: "Montserrat", sans-serif;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  background-color: #0093e9;
+  height: inherit;
+  a {
+    text-decoration: none;
+    height: auto;
+    padding: 10px;
+  }
+  li {
+    color: black;
+    margin: 0 0.08rem;
+    font-size: 1rem;
+    list-style: none;
+    height: auto;
+    &:hover {
+      color: deeppink;
+    }
+  }
+  .current {
+    li {
+      border-bottom: 2px solid black;
+    }
+  }
+`;
